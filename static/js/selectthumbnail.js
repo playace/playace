06bfad2c7aa7e71b93e811412811ce4b0ccba919
Lisces <lisces@gmail.com>
2011-09-06 22:47:50 +0800

    clear to add new project

--- static/js/selectthumbnail.js
+++
@@ -1,7 +0,0 @@
-function ThumbnailItemChange(thumbnail) {
-	var obj_thumbnailimg = document.getElementById("thumbnailimg")
-	if (thumbnail.selectedIndex!=0){
-		thumbnailfn = thumbnail.options[thumbnail.selectedIndex].value
-		obj_thumbnailimg.src = "../../static/img/demoicon/" + thumbnailfn + ".jpg";
-	}
-}
